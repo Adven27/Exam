89bf8851c5522e9b0d7fb3d4bd84568fa0b3e96e
Николай Алексеевич Торопанов <NAToropanov.SBT@sberbank.ru>
2017-09-07 18:31:30 +0300

    gh-pages: remove old files

--- .utility/update-gh-pages.sh
+++ .utility/update-gh-pages.sh
@@ -10,6 +10,9 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/Adven27/Exam.git  gh-pages > /dev/null
 
   cd gh-pages
+  git rm -rf .
+  git commit -m "Travis build $TRAVIS_BUILD_NUMBER remove old files"
+
   cp -Rf $HOME/specs/* .
 
   git add -f .
