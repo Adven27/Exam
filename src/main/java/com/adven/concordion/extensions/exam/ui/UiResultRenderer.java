a2bfcf2ee1579bbe9031db735b3f8e7df7f5b956
Anton Dranitsyn <t0t0shc0@gmail.com>
2018-01-28 22:52:25 +0300

    fix folder for screenshot fix screenshot name for successful run step (multiple time same name) add description to successful result

--- src/main/java/com/adven/concordion/extensions/exam/ui/UiResultRenderer.java
+++ src/main/java/com/adven/concordion/extensions/exam/ui/UiResultRenderer.java
@@ -5,11 +5,15 @@ import com.codeborne.selenide.ex.UIAssertionError;
 import org.concordion.api.listener.*;
 
 import java.io.File;
+import java.util.concurrent.atomic.AtomicLong;
 
 import static com.adven.concordion.extensions.exam.html.Html.imageOverlay;
 import static com.codeborne.selenide.Selenide.screenshot;
 
 public class UiResultRenderer implements AssertEqualsListener, AssertTrueListener, AssertFalseListener {
+
+    private static final AtomicLong screenshotCounter = new AtomicLong();
+    private static final String DEFAULT_DESC = "No description";
 
     public void failureReported(AssertFailureEvent event) {
         Html s = new Html(event.getElement());
@@ -25,10 +29,20 @@ public class UiResultRenderer implements AssertEqualsListener, AssertTrueListene
         Html s = new Html(event.getElement());
         Html el = s.parent();
         String name = s.attr("name");
+        String desc = s.attr("desc");
         el.remove(s);
         el.childs(
-                imageOverlay(getPath(screenshot(name)), 360, name, "Step desc", "rest-success")
+                imageOverlay(getPath(screenshot(getFileName(name))), 360, name, checkAndGetDesc(desc), "rest-success")
         );
+    }
+
+    private String checkAndGetDesc(String desc) {
+        return desc == null ?  DEFAULT_DESC : desc;
+    }
+
+    private String getFileName(String name) {
+        long increment = screenshotCounter.incrementAndGet();
+        return increment + "-" + name;
     }
 
     private String getPath(String screenshot) {
