5ed483d42bc1a4847d299deafee768d9b73bd224
Anton Dranitsyn <t0t0shc0@gmail.com>
2018-07-19 01:00:28 +0300

    close manually created driver

---
+++ src/main/java/com/adven/concordion/extensions/exam/ui/ExamWebDriverContainer.java
@@ -0,0 +1,21 @@
+package com.adven.concordion.extensions.exam.ui;
+
+import com.codeborne.selenide.impl.WebDriverThreadLocalContainer;
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Extension for access protected methods.
+ * Because, if you create webdriver manually, you should close it.
+ */
+public class ExamWebDriverContainer extends WebDriverThreadLocalContainer {
+
+    /**
+     * Register driver for automatically close.
+     *
+     * @param driver web driver
+     */
+    public void registerDriver(WebDriver driver) {
+        setWebDriver(driver);
+        markForAutoClose(driver);
+    }
+}
