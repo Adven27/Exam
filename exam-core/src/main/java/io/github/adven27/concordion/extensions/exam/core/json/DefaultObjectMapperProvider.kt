45dcf9f922bea7bb03d4e35d2199b9b8e81bf748
n.toropanov <N.Toropanov@alfacapital.ru>
2022-02-25 08:40:47 +0100

    fix https://github.com/lukas-krecan/JsonUnit/issues/477

--- exam-core/src/main/java/io/github/adven27/concordion/extensions/exam/core/json/DefaultObjectMapperProvider.kt
+++ exam-core/src/main/java/io/github/adven27/concordion/extensions/exam/core/json/DefaultObjectMapperProvider.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonGenerator.Feature.WRITE_BIGDECIMAL_AS_PLAI
 import com.fasterxml.jackson.core.JsonParser.Feature.ALLOW_COMMENTS
 import com.fasterxml.jackson.core.JsonParser.Feature.ALLOW_SINGLE_QUOTES
 import com.fasterxml.jackson.core.JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES
+import com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_TRAILING_TOKENS
 import com.fasterxml.jackson.databind.DeserializationFeature.USE_BIG_DECIMAL_FOR_FLOATS
 import com.fasterxml.jackson.databind.ObjectMapper
 import net.javacrumbs.jsonunit.providers.Jackson2ObjectMapperProvider
@@ -12,6 +13,7 @@ open class DefaultObjectMapperProvider : Jackson2ObjectMapperProvider {
     private val mapper: ObjectMapper = ObjectMapper().apply {
         configure(USE_BIG_DECIMAL_FOR_FLOATS, true)
         configure(WRITE_BIGDECIMAL_AS_PLAIN, true)
+        configure(FAIL_ON_TRAILING_TOKENS, true)
     }
     private val lenientMapper: ObjectMapper = ObjectMapper().apply {
         configure(USE_BIG_DECIMAL_FOR_FLOATS, true)
@@ -19,6 +21,7 @@ open class DefaultObjectMapperProvider : Jackson2ObjectMapperProvider {
         configure(ALLOW_UNQUOTED_FIELD_NAMES, true)
         configure(ALLOW_COMMENTS, true)
         configure(ALLOW_SINGLE_QUOTES, true)
+        configure(FAIL_ON_TRAILING_TOKENS, true)
     }
 
     override fun getObjectMapper(lenient: Boolean) = if (lenient) lenientMapper else mapper
